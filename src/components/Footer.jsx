470305961cb427230e7c7196877a9c504960257c
onurbeht <brunnobatista25@gmail.com>
2024-06-24 20:10:43 -0300

    New: Footer and Navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="min-w-full h-[10vh] flex justify-center items-center bg-zinc-700 text-zinc-200 ">
+      <span>
+        Desafio oak - Criado por{" "}
+        <a
+          href="https://github.com/onurbeht"
+          target="_blank"
+          className="underline"
+        >
+          Bruno Oliveira
+        </a>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Button } from "./ui/button";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="min-w-full h-[10vh] flex justify-between items-center bg-zinc-700 text-zinc-200 ">
+      <span className="font-bold text-lg pl-4">
+        <Link to="/">Oak desafio</Link>
+      </span>
+      <span className="font-bold text-lg pr-4">
+        <Button variant="ghost">
+          <Link to="/novo-produto">Novo Produto</Link>
+        </Button>
+      </span>
+    </nav>
+  );
 };
 
 export default Navbar;

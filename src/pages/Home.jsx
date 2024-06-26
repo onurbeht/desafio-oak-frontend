106a7c8737bcbb20cf305521c7f015edf6804ee6
onurbeht <brunnobatista25@gmail.com>
2024-06-25 21:15:56 -0300

    Fix: Fetch and Card layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -5,40 +6,71 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useFetchProducts } from "@/hooks/useFetchProducts";
-import React from "react";
+import React, { useLayoutEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  const { products } = useFetchProducts();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  console.log(products);
+  const url = import.meta.env.VITE_API;
+
+  //console.log(products);
+
+  useLayoutEffect(() => {
+    setLoading(true);
+    fetch(url, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => console.log(error));
+
+    setLoading(false);
+  }, []);
+
+  if (loading) {
+    return <p>Carregando...</p>;
+  }
 
   return (
     <main className="min-h-[80vh] max-w-full bg-zinc-200 text-zinc-700 container">
-      {products.length === 0 ? (
-        <div>Nenhum produto cadastrado!</div>
-      ) : (
+      {products.length > 0 ? (
         <>
-          <div className="flex justify-center items-center py-2 font-bold text-2xl border-b-2 border-zinc-700">
+          <div className="flex justify-between py-2 font-bold text-2xl border-b-2 border-zinc-700">
             <h1>Lista de produtos cadastrados</h1>
           </div>
-          <section className="flex gap-2 flex-wrap pt-2">
+          <section className="flex gap-2 flex-col items-center py-2">
             {products.map((prod) => (
-              <Card className="min-w-1/3 min-h-max" key={prod.id}>
+              <Card className="w-3/4" key={prod.id}>
                 <CardHeader>
                   <CardTitle>{prod.name}</CardTitle>
                   <CardDescription>{prod.info}</CardDescription>
                 </CardHeader>
-                <CardContent className="flex justify-between">
-                  <p className="font-bold">R$ {prod.priceInCents / 100}</p>
-                  <p className="underline">
-                    {prod.available ? "Disponivel" : "Não disponivel"}
-                  </p>
+                <CardContent className="flex flex-col justify-between">
+                  <div className="flex justify-between">
+                    <p className="font-bold">R$ {prod.priceInCents / 100}</p>
+                    <p className="underline">
+                      {prod.available ? "Disponivel" : "Não disponivel"}
+                    </p>
+                  </div>
+                  <div className="flex justify-center pt-2">
+                    <Button variant="outline" className="w-fit ">
+                      <Link to={`/produto/${prod.id}`}>Ver mais</Link>
+                    </Button>
+                  </div>
                 </CardContent>
               </Card>
             ))}
           </section>
         </>
+      ) : (
+        <div>Nenhum produto cadastrado!</div>
       )}
     </main>
   );

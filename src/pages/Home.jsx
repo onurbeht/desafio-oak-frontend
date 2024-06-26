51f5154d815642703ab7655cbb808af2cd21da7a
onurbeht <brunnobatista25@gmail.com>
2024-06-26 16:00:05 -0300

    New: Sort method

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,20 +6,61 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import React, { useLayoutEffect, useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  DotsVerticalIcon,
+  PinBottomIcon,
+  PinTopIcon,
+} from "@radix-ui/react-icons";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [asc, setAsc] = useState(true);
+  const [desc, setDesc] = useState(false);
+  const [direction, setDirection] = useState("");
 
-  const url = import.meta.env.VITE_API;
+  const api = import.meta.env.VITE_API;
 
-  //console.log(products);
+  useEffect(() => {
+    if (asc) {
+      setDirection("ASC");
+    } else {
+      setDirection("DESC");
+    }
+  }, [asc, desc]);
+
+  useEffect(() => {
+    const sortedUrl = `${api}?direction=${direction}`;
+
+    setLoading(true);
+    fetch(sortedUrl, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => console.log(error));
+
+    setLoading(false);
+  }, [direction]);
 
   useLayoutEffect(() => {
     setLoading(true);
-    fetch(url, {
+    fetch(api, {
       method: "GET",
       headers: {
         "Content-type": "application/json",
@@ -34,16 +75,51 @@ const Home = () => {
     setLoading(false);
   }, []);
 
-  if (loading) {
-    return <p>Carregando...</p>;
-  }
-
   return (
     <main className="min-h-[80vh] max-w-full bg-zinc-200 text-zinc-700 container">
-      {products.length > 0 ? (
+      {loading ? (
+        <p>Carregando...</p>
+      ) : (
         <>
-          <div className="flex justify-between py-2 font-bold text-2xl border-b-2 border-zinc-700">
+          <div className="flex justify-between items-center py-2 font-bold text-2xl border-b-2 border-zinc-700">
             <h1>Lista de produtos cadastrados</h1>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="icon">
+                  <DotsVerticalIcon />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Ordenação: Valor</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuCheckboxItem
+                  checked={asc}
+                  onClick={() => {
+                    setAsc(true);
+                    setDesc(false);
+                  }}
+                  disabled={asc}
+                >
+                  <span className=" flex gap-2 items-center">
+                    <PinBottomIcon />
+                    Crescrente
+                  </span>
+                </DropdownMenuCheckboxItem>
+                <DropdownMenuCheckboxItem
+                  checked={desc}
+                  onClick={() => {
+                    setDesc(true);
+                    setAsc(false);
+                  }}
+                  disabled={desc}
+                >
+                  <span className=" flex gap-2 items-center">
+                    <PinTopIcon />
+                    Decrescente
+                  </span>
+                </DropdownMenuCheckboxItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
           </div>
           <section className="flex gap-2 flex-col items-center py-2">
             {products.map((prod) => (
@@ -60,7 +136,11 @@ const Home = () => {
                     </p>
                   </div>
                   <div className="flex justify-center pt-2">
-                    <Button variant="outline" className="w-fit ">
+                    <Button
+                      variant="outline"
+                      className="w-fit "
+                      disabled={!prod.available}
+                    >
                       <Link to={`/produto/${prod.id}`}>Ver mais</Link>
                     </Button>
                   </div>
@@ -69,8 +149,6 @@ const Home = () => {
             ))}
           </section>
         </>
-      ) : (
-        <div>Nenhum produto cadastrado!</div>
       )}
     </main>
   );
